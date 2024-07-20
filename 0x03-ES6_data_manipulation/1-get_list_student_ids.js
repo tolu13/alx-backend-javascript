ca499b5bb664fcdda1ddb5aac90e03ebd3f68a97
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-07-20 09:04:52 +0000

    new func

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 function getListStudentIds(students) {
-  if(!Array.isArray(students)) {
+  if (!Array.isArray(students)) {
     return [];
   }
-  return students.map(student => student.id);
+  return students.map(student => (student.id));
 }
 export default getListStudentIds;
