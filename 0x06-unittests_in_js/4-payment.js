2abee2adba4b3e8d888afbbf943aeec47a81d2c0
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-07-25 07:31:51 +0000

    ok

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,6 +1,4 @@
 Utils = require('./utils');
-
-Utils = require('./utils');
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
     const output = Utils.calculateNumber('SUM', totalAmount, totalShipping);
     console.log(`The total is: ${output}`);
